4d1a9857cac993a1591b7bd7d9cf11f2adb36372
poneilldev <p.oneill.apc@gmail.com>
2015-04-28 17:18:51 -0600

    Cleaned up the code and tinkered with it

--- HardCodedClassifier.java
+++ HardCodedClassifier.java
@@ -10,32 +10,43 @@ import weka.core.converters.ConverterUtils.DataSource;
  */
 public class IrisData {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws Exception {
-        // TODO code application logic here
         
-        String file = "/Users/paul/Desktop/BYU-Idaho/Spring2015/CS450/iris.csv";
+    String file = "/Users/paul/Desktop/BYU-Idaho/Spring2015/CS450/iris.csv";
         
-         DataSource source = new DataSource(file);
-        Instances data = source.getDataSet();
+    DataSource source = new DataSource(file);
+    Instances data = source.getDataSet();
 
+    // we are looking for the last attribute
     if (data.classIndex() == -1){
         data.setClassIndex(data.numAttributes() - 1);
     }
         
     
+    data.randomize(new Random(1));
+    
+    // set training set to 70%
+    RemovePercentage remove = new RemovePercentage();
+    remove.setPercentage(30);
+    remove.setInputFormat(data);
+    Instances trainingSet = Filter.useFilter(data, remove);
+    
+    // set the rest for the testing set
+    remove.setInvertSelection(true);
+    Instances testSet = Filter.useFilter(data, remove);
+    
+    
     HardCodedClassifier classifier = new HardCodedClassifier();
-        classifier.buildClassifier(data);
+    classifier.buildClassifier(trainingSet);
         
-        Evaluation eval = new Evaluation(data);
         
+    Evaluation eval = new Evaluation(trainingSet);
+    eval.evaluateModel(classifier, testSet);
+    //eval.crossValidateModel(classifier, data, 10, new Random(1));
+        
+    System.out.println("Results: " + eval.toSummaryString());   
+    
     }
-    
-    
-    
-    
     
 }
 
